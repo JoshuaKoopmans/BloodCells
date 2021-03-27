79b7a39a96d3b4b91a47247fdb793d5f3c7c419d
Joshua Koopmans <joshua.abc@hotmail.com>
2021-03-27 14:09:03 +0100

    Added new layer to model && stops after 6000 epochs

--- scripts/model.py
+++ scripts/model.py
@@ -7,6 +7,7 @@ from skimage import img_as_float
 from skimage.feature import peak_local_max
 from datetime import datetime
 import os
+import sys
 import gc
 import argparse
 
@@ -30,14 +31,16 @@ os.mkdir(model_dir)
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels=2, out_channels=16, kernel_size=5, padding=28)
+        self.conv1 = nn.Conv2d(in_channels=2, out_channels=32, kernel_size=5, padding=30)
         self.pool = nn.MaxPool2d(2, 2)
-        self.bn1 = nn.BatchNorm2d(16)
-        self.conv2 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=5, dilation=3)
-        self.bn2 = nn.BatchNorm2d(32)
-        self.conv3 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=5, dilation=3)
+        self.bn1 = nn.BatchNorm2d(32)
+        self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=5, dilation=3)
+        self.bn2 = nn.BatchNorm2d(64)
+        self.conv3 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=5, dilation=3)
         self.bn3 = nn.BatchNorm2d(64)
         self.upsample = nn.Upsample(scale_factor=2, mode='bilinear')
+        self.conv6 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=5)
+        self.bn6 = nn.BatchNorm2d(64)
         self.conv4 = nn.Conv2d(in_channels=64, out_channels=2, kernel_size=5)
         self.conv5 = nn.Conv2d(in_channels=64, out_channels=1, kernel_size=5)
         self.softmax = nn.Softmax()
@@ -48,6 +51,7 @@ class Net(nn.Module):
         x = self.bn2(self.relu(self.conv2(x)))
         x = self.bn3(self.relu(self.conv3(x)))
         x = self.upsample(x)
+        x = self.bn6(self.relu(self.conv6(x)))
         y1 = self.conv4(x)
         y1 = self.softmax(y1)
         y2 = self.conv5(x)
@@ -132,6 +136,8 @@ def train(X, y, mini_batch_size: int):
                 evaluate(model, epoch)
                 torch.save(model.state_dict(), prefix + "convnet.pt")
                 model.train()
+            if epoch > 6000:
+                sys.exit(0)
             epoch += 1
 
 
