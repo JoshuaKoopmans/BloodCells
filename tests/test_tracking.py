14372fc302edac592c370b0e459fbc0a15c593df
Joshua Koopmans <joshua.koopmans@radboudumc.nl>
2021-06-19 01:51:38 +0200

    Modified tracking tests

--- tests/test_tracking.py
+++ tests/test_tracking.py
@@ -8,18 +8,18 @@ import cv2 as cv
 
 class TestTrackingRules(unittest.TestCase):
     def setUp(self):
-        self.test_gaussians = os.listdir("test_gaussians/")
+        self.test_gaussians = os.listdir("./tests/gaussian_images/")
         self.test_gaussians.sort(key=lambda x: int(x.split("_")[-1][:-4]))
         self.cell_1 = Cell(coordinates=np.array([610, 10]), video_type="test", median_background=None,
                            initial_frame_num=-1)
 
     def test_coordinates(self):
-        coordinates, _ = get_coordinates(cv.imread("../tests/test_gaussians/" + self.test_gaussians[1], 0) / 255)
+        coordinates, _ = get_coordinates(cv.imread("./tests/gaussian_images/" + self.test_gaussians[1], 0) / 255)
         self.assertEqual(len(coordinates), 9)
 
     def test_cell_initialization(self):
         cell_list = []
-        coordinates, _ = get_coordinates(cv.imread("../tests/test_gaussians/" + self.test_gaussians[2], 0) / 255)
+        coordinates, _ = get_coordinates(cv.imread("./tests/gaussian_images/" + self.test_gaussians[2], 0) / 255)
         create_new_cells(cell_list, coordinates=coordinates, video_type="test", background=None, frame_num=0)
         self.assertEqual(len(cell_list), 1)
 
@@ -27,7 +27,7 @@ class TestTrackingRules(unittest.TestCase):
         cell_list = []
 
         for n, frame in enumerate(self.test_gaussians[23:33]):
-            coordinates, _ = get_coordinates(cv.imread("../tests/test_gaussians/" + frame, 0) / 255)
+            coordinates, _ = get_coordinates(cv.imread("./tests/gaussian_images/" + frame, 0) / 255)
             create_new_cells(cell_list=cell_list, coordinates=coordinates, video_type="test", background=None, frame_num=n)
             update_cell_list(cell_list=cell_list, org_coordinates=coordinates, frame_width=640, frame_number=n)
 
@@ -36,8 +36,7 @@ class TestTrackingRules(unittest.TestCase):
 
     def test_cell_completion(self):
         for n, frame in enumerate(self.test_gaussians[:]):
-            coordinates, _ = get_coordinates(cv.imread("../tests/test_gaussians/" + frame, 0) / 255)
-            cv.imwrite("tt_{}.png".format(str(n)), _)
+            coordinates, _ = get_coordinates(cv.imread("./tests/gaussian_images/" + frame, 0) / 255)
         if self.cell_1.get_current_coordinate()[1] < (640 - CELL_JOURNEY_COMPLETION_THRESHOLD):
             self.cell_1.arrived(frame_num=42)
         self.assertEqual(self.cell_1.has_arrived(), True)
